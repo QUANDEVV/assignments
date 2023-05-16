e7e174952d4cf45aec8409754f76442222bed4e2
QUANDEVV <vercelploy@gmail.com>
2023-05-16 11:37:27 +0300

    Delete

--- dictionary.py
+++ dictionary.py
@@ -14,34 +14,22 @@ library = {
 
 }
 
-get_book = library.get("1")
-get_book["author"] = "calvin"
-
-print(get_book)
-
-
-
-
-
-
-# library = {
-#         '1':{
-#         'author': 'FaithK',
-#         'title': 'How to win',
-#         'year':2020,
-#         'ISBN': 100
-#         }, 
-#         '2':{
-#         'author': 'Kym',
-#         'title': 'How to Sleep',
-#         'year':2023,
-#         'ISBN': 10032
-#         },
-
-# }
-
+# UPDATE 
 # get_book = library.get("1")
-# get_book.update({"author": "hhhh"})
-
+   #OPTION 1 # get_book["author"] = "ELON MUSK"
+   #OPTION 2 # get_book.update({"author": "hhhh"})
 # print(get_book)
+
+
+
+# DELETE
+# get_book = library.get("1")
+# del get_book["author"]
+#print(get_book)
+
+
+
+
+
+
 
